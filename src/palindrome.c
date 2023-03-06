ab42ac8755be2d268ed1f5f06d2863714b579ac5
Red <ProjektRed@buaa.edn.cn>
2023-03-06 14:48:05 +0800

    E1

--- src/palindrome.c
+++ src/palindrome.c
@@ -1,9 +1,22 @@
 #include <stdio.h>
+int isPalindrome(int n) {
+	int temp = n;
+	int reverse = 0;
+	while (temp != 0) {
+		reverse = reverse * 10 + temp % 10;
+		temp /= 10;
+	}
+	if (reverse == n) {
+		return 1;
+	} else {
+		return 0;
+	}
+}
 int main() {
 	int n;
 	scanf("%d", &n);
 
-	if () {
+	if (isPalindrome(n)) {
 		printf("Y\n");
 	} else {
 		printf("N\n");
