83e161ef9bbc94386d922d1cb8bb98e2f9cee6b1
洪陈天一 <21371163@buaa.edu.cn>
2023-04-17 11:21:40 +0000

    fix times

--- kern/sched.c
+++ kern/sched.c
@@ -39,7 +39,7 @@ void schedule(int yield) {
     if (yield || count == 0 || e == NULL || e->env_status != ENV_RUNNABLE) {
         if (e != NULL) {
             TAILQ_REMOVE(&env_sched_list, e, env_sched_link);
-            user_time[e->env_user]++;
+            user_time[e->env_user] += e->env_pri;
         }
         if (e != NULL && e->env_status == ENV_RUNNABLE) {
             TAILQ_INSERT_TAIL(&env_sched_list, e, env_sched_link);
