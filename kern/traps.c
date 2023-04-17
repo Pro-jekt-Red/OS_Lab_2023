7d508920da75157c8e6a1ef5bd7377e0f2521710
洪陈天一 <21371163@buaa.edu.cn>
2023-04-17 12:02:12 +0000

    first

--- kern/traps.c
+++ kern/traps.c
@@ -17,6 +17,7 @@ void (*exception_handlers[32])(void) = {
     [1] = handle_mod,
     [8] = handle_sys,
 #endif
+    [11] = handle_ov,
 };
 
 /* Overview:
@@ -26,4 +27,20 @@ void (*exception_handlers[32])(void) = {
 void do_reserved(struct Trapframe *tf) {
 	print_tf(tf);
 	panic("Unknown ExcCode %2d", (tf->cp0_cause >> 2) & 0x1f);
+}
+
+void do_ov(struct Trapframe *tf) {
+	// 你需要在此处实现问题描述的处理要求
+    u_long pa = va2pa(curenv->env_pgdir, tf->cp0_epc);
+	u_int *kva = KADDR(pa);
+    if ((*kva >> 26) == 8) { // addi
+        tf->regs[(*kva >> 16) & 31] = tf->regs[(*kva >> 21) & 31] / 2 + (*kva >> 1 & ((1 << 15) - 1));
+        tf->cp0_epc += 4;
+    }
+    else if ((*kva & ((1 << 11) - 1)) == 32) { // add
+        printk("add ov handled\n");
+    }
+    else if ((*kva & ((1 << 11) - 1)) == 34) { // sub
+        printk("sub ov handled\n");
+    }
 }
