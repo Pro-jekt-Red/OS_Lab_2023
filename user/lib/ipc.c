7a83314b88d78891351e56551519f02d8def01ee
洪陈天一 <21371163@buaa.edu.cn>
2023-04-24 12:46:15 +0000

    debug

--- user/lib/ipc.c
+++ user/lib/ipc.c
@@ -58,8 +58,10 @@ void ipc_broadcast(u_int val, void * srcva, u_int perm) {
         struct Env *tmp = e;
         u_int fa = 0;
         while (tmp->env_parent_id) {
+			debugf("now %x, fa %x\n", tmp->env_id, tmp->env_parent_id)
             fa = tmp->env_parent_id;
             if (fa == curenv->env_id){
+				debugf("%x try send\n", e->env_id);
                 ipc_send(e->env_id, val, srcva, perm);
                 break;
             }
