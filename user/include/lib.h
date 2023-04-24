fc8d218ce92ec460353f72fffd38faa1a9ba8b13
洪陈天一 <21371163@buaa.edu.cn>
2023-04-24 11:42:28 +0000

    first try

--- user/include/lib.h
+++ user/include/lib.h
@@ -73,6 +73,9 @@ int syscall_read_dev(void *, u_int, u_int);
 void ipc_send(u_int whom, u_int val, const void *srcva, u_int perm);
 u_int ipc_recv(u_int *whom, void *dstva, u_int *perm);
 
+// lab4-1-extra
+void ipc_broadcast(u_int val, void * srcva, u_int perm);
+
 // wait.c
 void wait(u_int envid);
 
