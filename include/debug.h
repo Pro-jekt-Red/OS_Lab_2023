3f8060b5ba91914cde9422779cd74f998c81563c
Red <ProjektRed@buaa.edu.cn>
2023-04-27 13:27:39 +0800

    fix warning

--- include/debug.h
+++ include/debug.h
@@ -2,12 +2,13 @@
 #include <pmap.h>
 #include <printk.h>
 
-#define TAILQ_DEBUG(INFO, elm, temp_elm, head, field) \
-    do{\
-        struct Env *temp_elm;\
-        printk(#INFO" %x %s\nNow envs: ", elm->env_id, (TAILQ_EMPTY(head)?"EMPTY":"NOT EMPTY"));\
-        TAILQ_FOREACH(temp_elm, head, field) {\
-            printk("id: %x ", temp_elm->env_id);\
-        }\
-        printk("\n");\
-    } while(0)
+#define TAILQ_DEBUG(INFO, elm, temp_elm, head, field)                                              \
+    do {                                                                                           \
+        struct Env *temp_elm;                                                                      \
+        printk(#INFO " %x %s\nNow envs: ", elm->env_id,                                            \
+               (TAILQ_EMPTY(head) ? "EMPTY" : "NOT EMPTY"));                                       \
+        TAILQ_FOREACH (temp_elm, head, field) {                                                    \
+            printk("id: %x ", temp_elm->env_id);                                                   \
+        }                                                                                          \
+        printk("\n");                                                                              \
+    } while (0)
--- kern/syscall_all.c
+++ kern/syscall_all.c
@@ -262,9 +262,7 @@ int sys_exofork(void) {
 
     /* Step 1: Allocate a new env using 'env_alloc'. */
     /* Exercise 4.9: Your code here. (1/4) */
-    if (env_alloc(&e, curenv->env_id) < 0) {
-        return -E_NO_FREE_ENV;
-    }
+    try(env_alloc(&e, curenv->env_id));
 
     /* Step 2: Copy the current Trapframe below 'KSTACKTOP' to the new env's 'env_tf'. */
     /* Exercise 4.9: Your code here. (2/4) */
